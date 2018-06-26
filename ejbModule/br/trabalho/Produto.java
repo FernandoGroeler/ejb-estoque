099e9917ee24a470f1616bc792f6c20370a92347
RafaelGuzzo <rafaajuda@hotmail.com>
2018-06-26 20:36:15 -0300

    .

--- ejbModule/br/trabalho/Produto.java
+++ ejbModule/br/trabalho/Produto.java
@@ -1,32 +1,41 @@
 package br.trabalho;
 
-public class Produto {
+import java.io.Serializable;
+
+public class Produto implements Serializable {
 	private int id;
 	private String descricao;
 	private double preco;
 	private double estoque;
-	
+
 	public int getId() {
 		return id;
 	}
+
 	public void setId(int id) {
 		this.id = id;
 	}
+
 	public String getDescricao() {
 		return descricao;
 	}
+
 	public void setDescricao(String descricao) {
 		this.descricao = descricao;
 	}
+
 	public double getPreco() {
 		return preco;
 	}
+
 	public void setPreco(double preco) {
 		this.preco = preco;
 	}
+
 	public double getEstoque() {
 		return estoque;
 	}
+
 	public void setEstoque(double estoque) {
 		this.estoque = estoque;
 	}
